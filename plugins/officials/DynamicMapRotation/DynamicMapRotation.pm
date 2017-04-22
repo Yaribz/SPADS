fb22be1b80ae8355281ce86f056338ef25aeee3e
Yaribz <yaribzh@gmail.com>
2017-04-22 17:32:47 +0200

    plugin DynamicMapRotation: version 0.2
    fix dynamic map rotation not working when battle is empty and default
    preset has been auto-restored

--- plugins/officials/DynamicMapRotation/DynamicMapRotation.pm
+++ plugins/officials/DynamicMapRotation/DynamicMapRotation.pm
@@ -8,7 +8,7 @@ use SpadsPluginApi;
 
 no warnings 'redefine';
 
-my $pluginVersion='0.1';
+my $pluginVersion='0.2';
 my $requiredSpadsVersion='0.11.5';
 
 sub getVersion { return $pluginVersion; }
@@ -25,7 +25,8 @@ sub new {
                                     nbPlayers => 0,
                                     currentNbTeams => 0,
                                     currentTeamSize => 0,
-                                    startPosType => 0 } };
+                                    startPosType => 0 },
+               previousRotationType => undef };
   bless($self,$class);
   return undef unless($self->onReloadConf());
   slog("Plugin loaded (version $pluginVersion)",3);
@@ -40,7 +41,7 @@ sub onReloadConf {
   return 0 unless(%{$p_rules});
   foreach my $p_mapListRule (@{$p_rules->{''}}) {
     return 0 unless($#{$p_mapListRule} == 1 && %{$p_mapListRule->[1]} && exists $p_mapListRule->[1]->{rotationMapList});
-    if(! exists $spads->{mapLists}->{$p_mapListRule->[1]->{rotationMapList}}) {
+    if(! exists $spads->{mapLists}{$p_mapListRule->[1]->{rotationMapList}}) {
       slog("Invalid map list \"$p_mapListRule->[1]->{rotationMapList}\" (referenced in DynamicMapRotation.conf but not defined in mapLists.conf)",1);
       return 0;
     }
@@ -71,14 +72,15 @@ sub eventLoop {
                            nbPlayers => $nbHumanPlayers+$nbAiBots,
                            currentNbTeams => $currentNbTeams,
                            currentTeamSize => $currentTeamSize,
-                           startPosType => $spads->{bSettings}->{startpostype} };
+                           startPosType => $spads->{bSettings}{startpostype} };
   my $needNewCheck=0;
   foreach my $contextData (keys %{$p_currentContext}) {
-    if($p_currentContext->{$contextData} != $self->{previousContext}->{$contextData}) {
+    if($p_currentContext->{$contextData} != $self->{previousContext}{$contextData}) {
       $needNewCheck=1;
       last;
     }
   }
+  $needNewCheck=1 unless(defined $self->{previousRotationType} && $p_spadsConf->{rotationType} eq $self->{previousRotationType});
   return unless($needNewCheck);
   $self->{previousContext}=$p_currentContext;
   my $p_mapLists=SpadsConf::findMatchingData($p_currentContext,$self->{rules});
@@ -87,18 +89,19 @@ sub eventLoop {
   }else{
     $p_spadsConf->{rotationType}='map';
   }
-  $spads->{conf}->{rotationType}=$p_spadsConf->{rotationType};
+  $spads->{conf}{rotationType}=$p_spadsConf->{rotationType};
+  $self->{previousRotationType}=$p_spadsConf->{rotationType};
 }
 
 
 sub getCurrentNumberOfPlayers {
   my ($nbHumanPlayers,$nbAiBots)=(0,0);
   my $lobby=getLobbyInterface();
-  my $p_bUsers=$lobby->{battle}->{users};
+  my $p_bUsers=$lobby->{battle}{users};
   foreach my $bUser (keys %{$p_bUsers}) {
-    ++$nbHumanPlayers if(defined $p_bUsers->{$bUser}->{battleStatus} && $p_bUsers->{$bUser}->{battleStatus}->{mode});
+    ++$nbHumanPlayers if(defined $p_bUsers->{$bUser}{battleStatus} && $p_bUsers->{$bUser}{battleStatus}{mode});
   }
-  my @bots=keys %{$lobby->{battle}->{bots}};
+  my @bots=keys %{$lobby->{battle}{bots}};
   $nbAiBots=$#bots+1;
   return ($nbHumanPlayers,$nbAiBots);
 }
