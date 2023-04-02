cca8b3ba701a48b56df0a34936916b9917797f50
Yaribz <yaribzh@gmail.com>
2023-04-02 17:10:18 +0200

    update getModOptions for new unitsync interface

--- src/getDefaultModOptions.pl
+++ src/getDefaultModOptions.pl
@@ -3,7 +3,7 @@
 # This program prints the default mod options of each Spring mod installed,
 # using the unitsync library.
 #
-# Copyright (C) 2008-2021  Yann Riou <yaribzh@gmail.com>
+# Copyright (C) 2008-2023  Yann Riou <yaribzh@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -31,26 +31,23 @@ use List::Util qw'any all none notall';
 
 use lib $FindBin::Bin;
 
-our $VERSION=0.7;
+our $VERSION=0.8;
 
-my %optionTypes = (
-  0 => 'error',
-  1 => 'bool',
-  2 => 'list',
-  3 => 'number',
-  4 => 'string',
-  5 => 'section'
-);
+use constant {
+  MSWIN32 => $^O eq 'MSWin32',
+  DARWIN => $^O eq 'darwin',
+};
 
-my $win=$^O eq 'MSWin32';
-my $macOs=$^O eq 'darwin';
-my $dynLibSuffix=$win?'dll':($macOs?'dylib':'so');
-my $unitsyncLibName=($win?'':'lib')."unitsync.$dynLibSuffix";
-my $pathSep=$win?';':':';
-my $cwd=cwd();
-my @originalArgv=@ARGV;
+use constant {
+  UNITSYNC_LIB_NAME => (MSWIN32 ? '' : 'lib').'unitsync.'.(MSWIN32 ? 'dll' : (DARWIN ? 'dylib' : 'so')),
+};
 
-if($win) {
+my @OPTION_TYPES=qw'error bool list number string section';
+
+my $PATH_SEP=MSWIN32?';':':';
+my $CWD=cwd();
+
+if(MSWIN32) {
   eval 'use Win32';
   die "\nWin32::API module version 0.73 or superior is required.\nPlease update your Perl installation (Perl 5.16.2 or superior is recommended)\n"
       unless(eval { require Win32::API; Win32::API->VERSION(0.73); 1; });
@@ -59,22 +56,19 @@ if($win) {
 }
 
 sub printUsageAndExit {
-
-  my $unitsyncPart1=$macOs?'':' [-u <unitsyncPath>|--unitsync=<unitsyncPath>]';
-  my $unitsyncPart2=$macOs?'':"\n      -u <unitsyncPath>|--unitsync=<unitsyncPath> : unitsync library path";
-
   print <<EOM;
 Usage:
 
   $0 -h|--help
     Prints help and exits
 
-  $0 [-c|--comments] [-s|--spads] [-o <file>|--output=<file>]$unitsyncPart1 [-d <springDataDir>|--datadir=<springDataDir>]
+  $0 [-c|--comments] [-s|--spads] [-o <file>|--output=<file>] [-u <unitsyncPath>|--unitsync=<unitsyncPath>] [-d <springDataDir>|--datadir=<springDataDir>]
     Generates the modoptions list for the games found in the Spring data directories.
     Optional command line parameters:
       -c|--comments             : includes comments explaining each mod option in the output
       -s|--spads                : generates output in SPADS format (ready to be copy-pasted in the battlePresets.conf file)
-      -o <file>|--output=<file> : writes output into <file> instead of stdout$unitsyncPart2
+      -o <file>|--output=<file> : writes output into <file> instead of stdout
+      -u <unitsyncPath>|--unitsync=<unitsyncPath> : unitsync library path
       -d <springDataDir>|--datadir=<springDataDir> : Spring data directory (this option can be provided multiple times to load several Spring data directories)
 
 EOM
@@ -96,7 +90,7 @@ EOM
 sub isAbsolutePath {
   my $fileName=shift;
   my $fileSpecRes=File::Spec->file_name_is_absolute($fileName);
-  return $fileSpecRes == 2 if($win);
+  return $fileSpecRes == 2 if(MSWIN32);
   return $fileSpecRes;
 }
 
@@ -113,16 +107,16 @@ sub isAbsoluteFilePath {
 
 sub areSamePaths {
   my ($p1,$p2)=map {File::Spec->canonpath($_)} @_;
-  ($p1,$p2)=map {lc($_)} ($p1,$p2) if($win);
+  ($p1,$p2)=map {lc($_)} ($p1,$p2) if(MSWIN32);
   return $p1 eq $p2;
 }
 
 sub setEnvVarFirstPaths {
   my ($varName,@firstPaths)=@_;
   my $needRestart=0;
-  die "Unable to handle path containing \"$pathSep\" character!\n" if(any {index($_,$pathSep) != -1} @firstPaths);
+  die "Unable to handle path containing \"$PATH_SEP\" character!\n" if(any {index($_,$PATH_SEP) != -1} @firstPaths);
   $ENV{"SPADS_$varName"}=$ENV{$varName}//'_UNDEF_' unless(exists $ENV{"SPADS_$varName"});
-  my @currentPaths=split(/$pathSep/,$ENV{$varName}//'');
+  my @currentPaths=split(/$PATH_SEP/,$ENV{$varName}//'');
   $needRestart=1 if($#currentPaths < $#firstPaths);
   if(! $needRestart) {
     for my $i (0..$#firstPaths) {
@@ -133,12 +127,12 @@ sub setEnvVarFirstPaths {
     }
   }
   if($needRestart) {
-    my @origPaths=$ENV{"SPADS_$varName"} eq '_UNDEF_' ? () : split(/$pathSep/,$ENV{"SPADS_$varName"});
+    my @origPaths=$ENV{"SPADS_$varName"} eq '_UNDEF_' ? () : split(/$PATH_SEP/,$ENV{"SPADS_$varName"});
     my @newPaths;
     foreach my $path (@origPaths) {
       push(@newPaths,$path) unless(any {areSamePaths($path,$_)} @firstPaths);
     }
-    $ENV{$varName}=join($pathSep,@firstPaths,@newPaths);
+    $ENV{$varName}=join($PATH_SEP,@firstPaths,@newPaths);
   }
   return $needRestart;
 }
@@ -147,23 +141,6 @@ sub exportWin32EnvVar {
   my $envVar=shift;
   my $envVarDef="$envVar=".($ENV{$envVar}//'');
   die "Unable to export environment variable definition \"$envVarDef\"" unless(_putenv($envVarDef) == 0);
-}
-
-sub escapeWin32Parameter {
-  my $arg = shift;
-  $arg =~ s/(\\*)"/$1$1\\"/g;
-  if($arg =~ /[ \t]/) {
-    $arg =~ s/(\\*)$/$1$1/;
-    $arg = "\"$arg\"";
-  }
-  return $arg;
-}
-
-sub portableExec {
-  my ($program,@params)=@_;
-  my @args=($program,@params);
-  @args=map {escapeWin32Parameter($_)} @args if($win);
-  return exec {$program} @args;
 }
 
 my $generateComments;
@@ -177,14 +154,14 @@ my %optionsHash=('help' => sub { printHelpAndExit() },
                  'comments!' => \$generateComments,
                  'spads!' => \$spadsFormat,
                  'output=s' => \$outputFile,
-                 'datadir=s' => \@dataDirs);
-$optionsHash{'unitsync=s'}=\$unitsyncPath unless($macOs);
+                 'datadir=s' => \@dataDirs,
+                 'unitsync=s' => \$unitsyncPath);
 printUsageAndExit() unless(GetOptions(%optionsHash));
 
 my @unrecognizedParams;
 foreach my $arg (@ARGV) {
   $arg=File::Spec->rel2abs($arg) if(-e $arg);
-  $unitsyncPath//=$arg if(! $macOs && isAbsoluteFilePath($arg,$unitsyncLibName));
+  $unitsyncPath//=$arg if(isAbsoluteFilePath($arg,UNITSYNC_LIB_NAME));
   if(-d $arg) {
     push(@dataDirs,$arg);
   }else{
@@ -195,7 +172,7 @@ if(@unrecognizedParams) {
   print 'Invalid parameter: "'.join('", "',@unrecognizedParams)."\"\n";
   printUsageAndExit();
 }
-@dataDirs=split(/$pathSep/,join($pathSep,@dataDirs));
+@dataDirs=split(/$PATH_SEP/,join($PATH_SEP,@dataDirs));
 foreach my $dataDir (@dataDirs) {
   if(-d $dataDir) {
     $dataDir=File::Spec->rel2abs($dataDir);
@@ -208,19 +185,68 @@ if(@unrecognizedParams) {
   printUsageAndExit();
 }
 
+my $unitsyncDir;
 if(defined $unitsyncPath) {
   $unitsyncPath=File::Spec->rel2abs($unitsyncPath) if(-e $unitsyncPath);
-  my $fullUnitsyncPath=isAbsoluteFilePath($unitsyncPath,$unitsyncLibName);
+  my $fullUnitsyncPath=isAbsoluteFilePath($unitsyncPath,UNITSYNC_LIB_NAME);
   if(! $fullUnitsyncPath) {
     print "Invalid unitsync path\n";
     printUsageAndExit();
   }
-  my $unitsyncDir=File::Spec->canonpath((fileparse($fullUnitsyncPath))[1]);
-  if($win) {
-    setEnvVarFirstPaths('PATH',$unitsyncDir);
+  $unitsyncDir=File::Spec->canonpath((fileparse($fullUnitsyncPath))[1]);
+}
+
+if(@dataDirs) {
+  setEnvVarFirstPaths('SPRING_DATADIR',@dataDirs);
+  exportWin32EnvVar('SPRING_DATADIR') if(MSWIN32);
+}
+
+$ENV{SPRING_WRITEDIR}=$CWD unless(areSamePaths($CWD,$ENV{SPRING_WRITEDIR}//''));
+exportWin32EnvVar('SPRING_WRITEDIR') if(MSWIN32);
+
+eval {require PerlUnitSync};
+die "ERROR - Unable to load PerlUnitSync module - $@" if($@);
+
+my $unitsync = eval {PerlUnitSync->new($unitsyncDir)};
+if($@) {
+  if($unitsyncPath) {
+    die "ERROR - $@";
   }else{
-    portableExec($^X,$0,@originalArgv) if(setEnvVarFirstPaths('LD_LIBRARY_PATH',$unitsyncDir));
+    print "ERROR - Failed to load unitsync library - try specifying the unitsync library path as command line parameter\n";
+    printUsageAndExit();
   }
+}
+die "ERROR - Failed to load unitsync library - unknown error\n" unless(defined $unitsync);
+
+if(! $unitsync->Init(0,0)) {
+  while(my $unitSyncErr=$unitsync->GetNextError()) {
+    chomp($unitSyncErr);
+    print("ERROR - UnitSync error: $unitSyncErr\n");
+  }
+  my $errorMsg='ERROR - Unable to initialize UnitSync library';
+  $errorMsg.=' (try specifying the Spring data directory as command line parameter)' unless(@dataDirs);
+  print $errorMsg."\n";
+  exit 0;
+}
+
+my $nbMods = $unitsync->GetPrimaryModCount();
+if(! $nbMods) {
+  print("ERROR - No Spring mod found\n");
+  $unitsync->UnInit();
+  exit 0;
+}
+
+my @availableMods=();
+for my $modNb (0..($nbMods-1)) {
+  my $nbInfo = $unitsync->GetPrimaryModInfoCount($modNb);
+  my $modName='';
+  for my $infoNb (0..($nbInfo-1)) {
+    next if($unitsync->GetInfoKey($infoNb) ne 'name');
+    $modName=$unitsync->GetInfoValueString($infoNb);
+    last;
+  }
+  my $modArchive = $unitsync->GetPrimaryModArchive($modNb);
+  $availableMods[$modNb]={name=>$modName,archive=>$modArchive};
 }
 
 if($outputFile) {
@@ -232,14 +258,6 @@ if($outputFile) {
   $outputHandle->autoflush(1);
 }
 
-if(@dataDirs) {
-  setEnvVarFirstPaths('SPRING_DATADIR',@dataDirs);
-  exportWin32EnvVar('SPRING_DATADIR') if($win);
-}
-
-$ENV{SPRING_WRITEDIR}=$cwd unless(areSamePaths($cwd,$ENV{SPRING_WRITEDIR}//''));
-exportWin32EnvVar('SPRING_WRITEDIR') if($win);
-
 sub printRes {
   my $s=shift;
   if(defined $outputHandle) {
@@ -247,47 +265,6 @@ sub printRes {
   }else{
     print $s;
   }
-}
-
-eval "use PerlUnitSync";
-if ($@) {
-  print "ERROR - Unable to load PerlUnitSync module ($@)\n";
-  if(! $macOs && ! $unitsyncPath) {
-    print "ERROR - Try specifying the unitsync library path as command line parameter\n";
-    printUsageAndExit();
-  }
-  exit 1;
-}
-
-if(! PerlUnitSync::Init(0,0)) {
-  while(my $unitSyncErr=PerlUnitSync::GetNextError()) {
-    chomp($unitSyncErr);
-    print("ERROR - UnitSync error: $unitSyncErr\n");
-  }
-  my $errorMsg='ERROR - Unable to initialize UnitSync library';
-  $errorMsg.=' (try specifying the Spring data directory as command line parameter)' unless(@dataDirs);
-  print $errorMsg."\n";
-  exit 0;
-}
-
-my $nbMods = PerlUnitSync::GetPrimaryModCount();
-if(! $nbMods) {
-  print("ERROR - No Spring mod found\n");
-  PerlUnitSync::UnInit();
-  exit 0;
-}
-
-my @availableMods=();
-for my $modNb (0..($nbMods-1)) {
-  my $nbInfo = PerlUnitSync::GetPrimaryModInfoCount($modNb);
-  my $modName='';
-  for my $infoNb (0..($nbInfo-1)) {
-    next if(PerlUnitSync::GetInfoKey($infoNb) ne 'name');
-    $modName=PerlUnitSync::GetInfoValueString($infoNb);
-    last;
-  }
-  my $modArchive = PerlUnitSync::GetPrimaryModArchive($modNb);
-  $availableMods[$modNb]={name=>$modName,archive=>$modArchive};
 }
 
 sub getRangeStepFromBoundaries {
@@ -305,10 +282,12 @@ sub printModOptions {
   my ($p_modOptions,$section)=@_;
   $section='' unless(defined $section);
   my @modOptions=@{$p_modOptions};
+  my $nothingPrinted=1;
   foreach my $optionIdx (0..$#modOptions) {
     my $p_option=$modOptions[$optionIdx];
     next if(($p_option->{section} && lc($section) ne lc($p_option->{section}))
             || (! $p_option->{section} && $section ne ''));
+    $nothingPrinted=0;
     my $comment="$p_option->{name}: $p_option->{description}";
     if($p_option->{type} eq "error") {
       printRes("\n#ERROR: $comment\n");
@@ -355,6 +334,7 @@ sub printModOptions {
       printRes("\n#ERROR: unknown mod option !\n");
     }
   }
+  printRes("\n") if($generateComments && $nothingPrinted && $section ne '');
 }
 
 sub formatNumber {
@@ -368,8 +348,8 @@ sub formatNumber {
 }
 
 for my $modNb (0..$#availableMods) {
-  PerlUnitSync::RemoveAllArchives();
-  PerlUnitSync::AddAllArchives($availableMods[$modNb]->{archive});
+  $unitsync->RemoveAllArchives();
+  $unitsync->AddAllArchives($availableMods[$modNb]->{archive});
   my $modSection=$availableMods[$modNb]->{name};
   $modSection=~s/[^\w]/_/g;
   printRes("[$modSection]\n");
@@ -379,34 +359,34 @@ for my $modNb (0..$#availableMods) {
     printRes("disabledunits:-*\n");
     printRes("startpostype:2|0|1\n");
   }
-  my $nbModOptions = PerlUnitSync::GetModOptionCount();
+  my $nbModOptions = $unitsync->GetModOptionCount();
   my @modOptions;
   for my $optionIdx (0..($nbModOptions-1)) {
-    my %option=(name => PerlUnitSync::GetOptionName($optionIdx),
-                key => PerlUnitSync::GetOptionKey($optionIdx),
-                description => PerlUnitSync::GetOptionDesc($optionIdx),
-                type => $optionTypes{PerlUnitSync::GetOptionType($optionIdx)},
-                section => PerlUnitSync::GetOptionSection($optionIdx),
+    my %option=(name => $unitsync->GetOptionName($optionIdx),
+                key => $unitsync->GetOptionKey($optionIdx),
+                description => $unitsync->GetOptionDesc($optionIdx),
+                type => $OPTION_TYPES[$unitsync->GetOptionType($optionIdx)],
+                section => $unitsync->GetOptionSection($optionIdx),
                 default => "");
     $option{description}=~s/\n/ /g;
     if($option{type} eq "bool") {
-      $option{default}=PerlUnitSync::GetOptionBoolDef($optionIdx);
+      $option{default}=$unitsync->GetOptionBoolDef($optionIdx);
     }elsif($option{type} eq "number") {
-      $option{default}=formatNumber(PerlUnitSync::GetOptionNumberDef($optionIdx));
-      $option{numberMin}=formatNumber(PerlUnitSync::GetOptionNumberMin($optionIdx));
-      $option{numberMax}=formatNumber(PerlUnitSync::GetOptionNumberMax($optionIdx));
-      $option{numberStep}=formatNumber(PerlUnitSync::GetOptionNumberStep($optionIdx));
+      $option{default}=formatNumber($unitsync->GetOptionNumberDef($optionIdx));
+      $option{numberMin}=formatNumber($unitsync->GetOptionNumberMin($optionIdx));
+      $option{numberMax}=formatNumber($unitsync->GetOptionNumberMax($optionIdx));
+      $option{numberStep}=formatNumber($unitsync->GetOptionNumberStep($optionIdx));
     }elsif($option{type} eq "string") {
-      $option{default}=PerlUnitSync::GetOptionStringDef($optionIdx);
-      $option{stringMaxLen}=PerlUnitSync::GetOptionStringMaxLen($optionIdx);
+      $option{default}=$unitsync->GetOptionStringDef($optionIdx);
+      $option{stringMaxLen}=$unitsync->GetOptionStringMaxLen($optionIdx);
     }elsif($option{type} eq "list") {
-      $option{default}=PerlUnitSync::GetOptionListDef($optionIdx);
-      $option{listCount}=PerlUnitSync::GetOptionListCount($optionIdx);
+      $option{default}=$unitsync->GetOptionListDef($optionIdx);
+      $option{listCount}=$unitsync->GetOptionListCount($optionIdx);
       $option{list}=[];
       for my $listIdx (0..($option{listCount}-1)) {
-        my %item=(name => PerlUnitSync::GetOptionListItemName($optionIdx,$listIdx),
-                  description => PerlUnitSync::GetOptionListItemDesc($optionIdx,$listIdx),
-                  key => PerlUnitSync::GetOptionListItemKey($optionIdx,$listIdx));
+        my %item=(name => $unitsync->GetOptionListItemName($optionIdx,$listIdx),
+                  description => $unitsync->GetOptionListItemDesc($optionIdx,$listIdx),
+                  key => $unitsync->GetOptionListItemKey($optionIdx,$listIdx));
         $item{description}=~s/\n/ /g;
         push(@{$option{list}},\%item);
       }
@@ -418,6 +398,6 @@ for my $modNb (0..$#availableMods) {
   printRes("\n\n") if($generateComments);
 }
 
-PerlUnitSync::UnInit();
+$unitsync->UnInit();
 
 $outputHandle->close() if(defined $outputHandle);
